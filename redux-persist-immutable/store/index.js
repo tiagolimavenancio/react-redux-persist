9d0c4394a7ddbafb914b8f91e4519f72259c1a30
Tiago Lima <tiagolima@Tiagos-Mac-mini.local>
2019-09-23 14:27:02 -0300

    Persist a part of nested state

--- redux-persist-immutable/store/index.js
+++ redux-persist-immutable/store/index.js
@@ -1,8 +1,11 @@
-import { createStore, componse, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { autoRehydrate, persistStore } from 'redux-persist-immutable'
+import createFilter from '@actra-development-oss/redux-persist-transform-filter-immutable'
 
 import { fromJS } from 'immutable'
 import defaultState from ''
+
+const myFilter = createFilter('auth', ['userToken'])
 
 export function configureStore(initialState=fromJS(defaultState)) {
     const sagaMiddleware = createSagaMiddleware()
@@ -21,7 +24,9 @@ export function configureStore(initialState=fromJS(defaultState)) {
 }
 
 const persistor = persistStore(store, {
-    storage: AsyncStorage
+    storage: AsyncStorage,
+    whitelist: ['auth'],
+    transforms: [myFilter]
 })
 
 const store = configureStore()
